3fbd28190091d28ec6ac842427e82a1568d4280d
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-07-15 16:39:42 -0700

    initial populate added game board creation added token addition added player image placement added generate token function added css for token appearance

--- includes/main.js
+++ includes/main.js
@@ -4,10 +4,14 @@ var playerData = [
 	{ name: 'llama', image: 'images/llama.jpg'}
 ]
 var currentPlayer = 0;
-
+var squareClasses = ['whiteSquare', 'blackSquare'];
 //start up the game setup
 function initializeApp(){
-
+	var numberOfRows = 8;
+	var numberOfColumns = 8;
+	initializeClickHandlers();
+	createGameBoard(numberOfRows, numberOfColumns);
+	populateBoardWithTokens(playerData, numberOfRows);
 }
 
 //add click handlers to elements
@@ -15,15 +19,59 @@ function initializeClickHandlers(){
 
 }
 //create our 8x8 game board with alternating tiles
-function createGameBoard(){
-
+//input: rowCount and columnCount, number of rows and columns.  Not really used effectively yet
+//output: none
+//global: gameBoardArray, squareClasses
+function createGameBoard(rowCount, columnCount){
+	
+	var currentSquare = 0;
+	var gameBoardElement = document.querySelector('.board');
+	for(var row = 0; row< rowCount; row++){
+		gameBoardArray[row] = [];
+		for(var column = 0; column < columnCount; column++){
+			var square = document.createElement('div');
+			square.classList.add( 'square', squareClasses[currentSquare]);
+			currentSquare = 1 - currentSquare;
+			gameBoardElement.appendChild(square);
+			gameBoardArray[row][column] = square;
+		}
+		currentSquare = 1 - currentSquare;
+	}
+	console.log(gameBoardArray);
 }
 //create our tokens on the game board
-//input: array with player information, in this case player images and names
-//return: the array of dom elements to populate the game board
-//globals: game board multi dimensional array
-function populateBoardWithTokens(){
-
+//input: array with player information, in this case player images and names, row count
+//return: 
+//globals: game board multi dimensional array, squareClasses
+function populateBoardWithTokens(playerInfo, rowCount){
+	var playerImage = playerData[0].image;
+	var playerSquare = squareClasses[1];
+	for(var row = 0; row< 2; row++){
+		for(var column = 0; column < gameBoardArray[row].length; column++){
+			if(gameBoardArray[row][column].classList.contains( playerSquare )){
+				gameBoardArray[row][column].appendChild( generateToken(playerImage) );
+			}
+		}
+		
+	}
+	var playerImage = playerData[1].image;
+	for(var row = gameBoardArray.length-2; row< gameBoardArray.length ; row++){
+		for(var column = 0; column < gameBoardArray[row].length; column++){
+			if(gameBoardArray[row][column].classList.contains( playerSquare )){
+				gameBoardArray[row][column].appendChild( generateToken(playerImage) );
+			}
+		}
+	}
+}
+//add a token to the specifed location
+//intput: image to add to token
+//return: the token dom element
+//global: none
+function generateToken(tokenImage){
+	var token = document.createElement('div');
+	token.classList.add('token');
+	token.style.backgroundImage = 'url('+tokenImage+ ')';
+	return token;
 }
 
 //determine what moves the current chip has
@@ -73,7 +121,7 @@ function displayCurrentPlayer(){
 
 }
 
-
+initializeApp();
 
 
 
